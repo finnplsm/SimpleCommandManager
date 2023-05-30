ed90d64670a261a8e7e161c4be38459a88b7d338
finnplsm <finn.plassmeier@plassy.de>
2023-05-30 19:13:32 +0200

    Fixed registerCommands in SimpleCommandManager

--- src/main/java/de/finnp/simplecommandmanager/SimpleCommandManager.java
+++ src/main/java/de/finnp/simplecommandmanager/SimpleCommandManager.java
@@ -1,11 +1,8 @@
 package de.finnp.simplecommandmanager;
 
 import org.bukkit.command.PluginCommand;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Objects;
@@ -21,7 +18,7 @@ public class SimpleCommandManager {
         setCommands(commands);
         setCommandApi(new CommandApi(getCommands()));
         registerCommands();
-        plugin.getLogger().info("Commands successfully integrated into the SimpleCommandManager");
+        plugin.getLogger().log(Level.INFO,"Commands successfully integrated into the SimpleCommandManager");
     }
 
     @NotNull
@@ -57,32 +54,16 @@ public class SimpleCommandManager {
                 if (!method.isAnnotationPresent(CommandHandler.class) ||
                         method.getParameterTypes().length != 1) continue;
                 getCommandApi().registerCommandHandler(command.getClass());
-                final File pluginFile = new File(plugin.getDataFolder(), "plugin.yml");
-                final YamlConfiguration pluginConfig = YamlConfiguration.loadConfiguration(pluginFile);
-                pluginConfig.set("commands."+method.getAnnotation(CommandHandler.class).value(),"");
+                PluginCommand pluginCommand=Objects.requireNonNull(getPlugin().getServer().getPluginCommand(method.getAnnotation(CommandHandler.class).value()));
+                pluginCommand.setExecutor(getCommandApi());
+                pluginCommand.setTabCompleter(getCommandApi());
                 if (method.isAnnotationPresent(CommandProperties.class)){
-                    pluginConfig.set("commands."+method.getAnnotation(CommandHandler.class).value()+"aliases",method.getAnnotation(CommandProperties.class).aliases());
-                    pluginConfig.set("commands."+method.getAnnotation(CommandHandler.class).value()+"usage",method.getAnnotation(CommandProperties.class).usage());
-                    pluginConfig.set("commands."+method.getAnnotation(CommandHandler.class).value()+"description",method.getAnnotation(CommandProperties.class).description());
-                    pluginConfig.set("commands."+method.getAnnotation(CommandHandler.class).value()+"permission",method.getAnnotation(CommandProperties.class).permission());
-                    pluginConfig.set("commands."+method.getAnnotation(CommandHandler.class).value()+"permission-message",method.getAnnotation(CommandProperties.class).permissionMessage());
-                    /*
                     pluginCommand.setUsage(method.getAnnotation(CommandProperties.class).usage().replaceAll("%label%",method.getAnnotation(CommandHandler.class).value()));
                     pluginCommand.setPermission(method.getAnnotation(CommandProperties.class).permission());
                     pluginCommand.setAliases(Arrays.asList(method.getAnnotation(CommandProperties.class).aliases()));
                     pluginCommand.setDescription(method.getAnnotation(CommandProperties.class).description());
                     pluginCommand.setPermissionMessage(method.getAnnotation(CommandProperties.class).permissionMessage());
-                    */
                 }
-                try {
-                    pluginConfig.save(pluginFile);
-                    getPlugin().getLogger().log(Level.FINE,pluginConfig.saveToString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                PluginCommand pluginCommand=Objects.requireNonNull(getPlugin().getServer().getPluginCommand(method.getAnnotation(CommandHandler.class).value()));
-                pluginCommand.setExecutor(getCommandApi());
-                pluginCommand.setTabCompleter(getCommandApi());
             }
         }
     }
